3fc32858bea4a9b3af5294a4ea6811662917136e
suvradeep sinharoy <suvradeep.test@outlook.com>
2023-07-23 23:23:07 +0530

    init 2nd push

--- MVCTrial/Program.cs
+++ MVCTrial/Program.cs
@@ -20,6 +20,6 @@ namespace MVCTrial
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-        /* test*/
+        /* test hello*/
     }
 }
